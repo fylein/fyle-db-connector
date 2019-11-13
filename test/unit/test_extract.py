2e15e13b352b35771eab0145a0019f61572c6f46
Sivaramakrishnan Narayanan <siva@fyle.in>
2019-11-13 22:56:23 +0530

    Bumped up test coverage to 45%

--- test/unit/test_extract.py
+++ test/unit/test_extract.py
@@ -16,6 +16,57 @@ def test_employees(fec):
     assert dbconn_table_num_rows(dbconn, 'fyle_extract_employees') == len(fyle_data), 'row count mismatch'
     assert len(ids) == 12, 'return value messed up'
 
+def test_settlements(fec):
+    dbconn = fec._FyleExtractConnector__dbconn
+    fyle = fec._FyleExtractConnector__connection
+    ids = fec.extract_settlements()
+    fyle_data = fyle.Settlements.get_all()
+    db_data = dbconn_table_row_dict(dbconn, 'fyle_extract_settlements')
+    assert dict_compare_keys(db_data, fyle_data[0]) == [], 'db table has some columns that fyle doesnt'
+    assert dbconn_table_num_rows(dbconn, 'fyle_extract_settlements') == len(fyle_data), 'row count mismatch'
+    assert len(ids) == 3, 'return value messed up'
+
+def test_reimbursements(fec):
+    dbconn = fec._FyleExtractConnector__dbconn
+    fyle = fec._FyleExtractConnector__connection
+    ids = fec.extract_reimbursements()
+    fyle_data = fyle.Reimbursements.get_all()
+    db_data = dbconn_table_row_dict(dbconn, 'fyle_extract_reimbursements')
+    assert dict_compare_keys(db_data, fyle_data[0]) == [], 'db table has some columns that fyle doesnt'
+    assert dbconn_table_num_rows(dbconn, 'fyle_extract_reimbursements') == len(fyle_data), 'row count mismatch'
+    assert len(ids) == 3, 'return value messed up'
+
+
+def test_expenses(fec):
+    dbconn = fec._FyleExtractConnector__dbconn
+    fyle = fec._FyleExtractConnector__connection
+    ids = fec.extract_expenses()
+    fyle_data = fyle.Expenses.get_all()
+    db_data = dbconn_table_row_dict(dbconn, 'fyle_extract_expenses')
+    assert dict_compare_keys(db_data, fyle_data[0]) == [], 'db table has some columns that fyle doesnt'
+    assert dbconn_table_num_rows(dbconn, 'fyle_extract_expenses') == len(fyle_data), 'row count mismatch'
+    assert len(ids) == 5, 'return value messed up'
+
+def test_categories(fec):
+    dbconn = fec._FyleExtractConnector__dbconn
+    fyle = fec._FyleExtractConnector__connection
+    ids = fec.extract_categories()
+    fyle_data = fyle.Categories.get()['data']
+    db_data = dbconn_table_row_dict(dbconn, 'fyle_extract_categories')
+    assert dict_compare_keys(db_data, fyle_data[0]) == [], 'db table has some columns that fyle doesnt'
+    assert dbconn_table_num_rows(dbconn, 'fyle_extract_categories') == len(fyle_data), 'row count mismatch'
+    assert len(ids) == 26, 'return value messed up'
+
+def test_projects(fec):
+    dbconn = fec._FyleExtractConnector__dbconn
+    fyle = fec._FyleExtractConnector__connection
+    ids = fec.extract_projects()
+    fyle_data = fyle.Projects.get()['data']
+    db_data = dbconn_table_row_dict(dbconn, 'fyle_extract_projects')
+    assert dict_compare_keys(db_data, fyle_data[0]) == [], 'db table has some columns that fyle doesnt'
+    assert dbconn_table_num_rows(dbconn, 'fyle_extract_projects') == len(fyle_data), 'row count mismatch'
+    assert len(ids) == 3, 'return value messed up'
+
 
 def test_empty(dbconn):
     fyle = get_mock_fyle_empty()
