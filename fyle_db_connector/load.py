354e589a04e0c6ba34155d4610722640af7c8a88
Sravan Kumar <sravan.kumar@fyle.in>
2020-01-03 14:41:41 +0530

    added return types to load functions

--- fyle_db_connector/load.py
+++ fyle_db_connector/load.py
@@ -4,7 +4,7 @@ FyleLoadConnector(): Connection between Fyle and Database
 
 import logging
 from os import path
-from typing import BinaryIO
+from typing import BinaryIO, List
 import pandas as pd
 
 
@@ -50,7 +50,7 @@ class FyleLoadConnector:
         self.logger.info('Excel file uploaded successfully.')
         return file_id
 
-    def load_tpa_export_batch(self, batch_id, file_path: str = None, file_id: str = None):
+    def load_tpa_export_batch(self, batch_id, file_path: str = None, file_id: str = None) -> List:
         """
         Load a TPA Export Batch in Fyle
         :param batch_id: Batch ID
@@ -62,7 +62,7 @@ class FyleLoadConnector:
                                            f"id = '{batch_id}' limit 1", con=self.__dbconn)
         if len(batches_df) == 0:
             self.logger.info('No such batch')
-            return
+            return []
 
         if not file_id:
             if file_path:
@@ -81,7 +81,7 @@ class FyleLoadConnector:
 
         if len(lineitems_df) == 0:
             self.logger.info('Batch %s has no lineitems, skipping', batch_id)
-            return
+            return []
 
         lineitems = lineitems_df.to_dict(orient='records')
 
@@ -89,8 +89,9 @@ class FyleLoadConnector:
         self.logger.info('Batch successfully upload. Uploading Line items.')
         self.__connection.Exports.post_batch_lineitems(batch_id, lineitems)
         self.logger.info('%s Lineitems successfully uploaded.', len(lineitems))
+        return batches_df['id'].to_list()
 
-    def load_tpa_exports(self, file_path: str = None, file_id: str = None) -> None:
+    def load_tpa_exports(self, file_path: str = None, file_id: str = None) -> List:
         """
         Load TPA Export Batches in Fyle
         :param file_path: Path of the export file
@@ -102,11 +103,12 @@ class FyleLoadConnector:
         batches_df = pd.read_sql_query(sql='select id from fyle_load_tpa_export_batches', con=self.__dbconn)
         if len(batches_df) == 0:
             self.logger.info('nothing to export')
-            return
+            return []
         batches = batches_df.to_dict(orient='records')
         self.logger.info('Pushing %d batches to Fyle', len(batches))
         for batch in batches:
             self.load_tpa_export_batch(batch_id=batch['id'], file_path=file_path, file_id=file_id)
+        return batches_df['id'].to_list()
 
     def load_file(self, file_name: str, file_data: BinaryIO, content_type: str) -> str:
         """
