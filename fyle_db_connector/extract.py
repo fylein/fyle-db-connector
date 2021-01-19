99d2a6eba67bc136435657e3ace773832fff7d8e
Sravan Kumar <sravan.kumar@fyle.in>
2021-01-19 23:41:28 +0530

    support employee custom fields

--- fyle_db_connector/extract.py
+++ fyle_db_connector/extract.py
@@ -60,7 +60,7 @@ class FyleExtractConnector:
 
         return []
 
-    def extract_employees(self) -> List[str]:
+    def extract_employees(self, extract_custom_fields: bool = True) -> List[str]:
         """
         Extract employees from Fyle
         :return: List of employee ids
@@ -84,6 +84,24 @@ class FyleExtractConnector:
             ]]
 
             df_employees.to_sql('fyle_extract_employees', self.__dbconn, if_exists='append', index=False)
+
+            custom_fields = []
+            for e in employees:
+                if 'custom_fields' in e and e['custom_fields']:
+                    for cp in e['custom_fields']:
+                        custom_fields.append({
+                            'employee_id': e['id'],
+                            'name': cp['custom_field_name'],
+                            'value': cp['custom_field_value']
+                        })
+
+            if extract_custom_fields:
+                if custom_fields:
+                    df_custom_fields = pd.DataFrame(custom_fields)
+                    df_custom_fields = df_custom_fields.astype('str')
+                    df_custom_fields.to_sql('fyle_extract_employee_custom_fields', self.__dbconn,
+                                            if_exists='append', index=False)
+
             return df_employees['id'].to_list()
 
         return []
